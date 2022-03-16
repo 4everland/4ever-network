592b267acc54322a5a556b2224a25dba4e979d0a
indirasieben <indirasiebenenv79@gmail.com>
2022-03-16 16:57:09 +0800

    chore:4ever-network init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-app-bar fixed app color="#FAFAFA" height="100">
+      <v-container class="d-flex align-center">
+        <v-btn text color="transparent" to="/" class="always-active mr-8">
+          <logo />
+        </v-btn>
+      </v-container>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import Logo from "../components/Logo.vue";
+
+export default {
+  components: { Logo },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.always-active {
+  opacity: 1 !important;
+}
+.v-btn--active,
+.nav-btn:hover {
+  color: #2a7eed;
+}
+/deep/ .v-btn__content {
+  opacity: 1 !important;
+}
+/deep/ .nav-btn .v-btn__content {
+  opacity: 1 !important;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+<style lang="less" scoped>
+.home {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+</style>
