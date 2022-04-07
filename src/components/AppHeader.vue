3c376aad3a87a721f3b48e006d75a48bc2c94daa
yepCing <yepCing@gmail.com>
2022-04-07 18:23:24 +0800

    bug fixed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,118 +1,142 @@
 <template>
-  <div>
-    <v-app-bar fixed app color="#FAFAFA" height="100">
-      <v-container class="d-flex align-center justify-space-between">
-        <v-btn text color="transparent" to="/" class="always-active mr-8">
-          <logo />
-        </v-btn>
-        <div class="d-flex align-center justify-space-between">
-          <div class="search-bar">
-            <input
-              class="search-input"
-              type="text"
-              v-model="searchValue"
-              @keydown.enter="handleSearch"
-              placeholder="Node/TeeReport/CID......"
-            />
-            <v-menu
-              :value="isShow"
-              :close-on-content-click="false"
-              @input="handleMenu"
-              transition="slide-y-transition"
-              attach
-              nudge-bottom="50"
-            >
-              <div v-if="isShow && status" class="content">
-                <div class="message-header d-flex align-center">
-                  <i class="header-line left"></i>
-                  <span>{{
-                    searchData.type == "BLOCK"
-                      ? "TeeReport"
-                      : searchData.type == "CID"
-                      ? "CID"
-                      : "NodeID"
-                  }}</span>
-                  <i class="header-line right"></i>
-                </div>
-                <div
-                  class="message-content-block d-flex flex-column"
-                  v-if="searchData.type == 'BLOCK'"
-                >
-                  <span class="block-info"
-                    ># {{ searchData.block.blockNumber }}</span
-                  >
-                  <span class="block-time">
-                    {{ searchData.block.createdAt }}</span
-                  >
-                </div>
-                <div
-                  class="message-content-cid"
-                  v-else-if="searchData.type == 'NODE'"
-                >
-                  <span class="message-cid">{{ searchData.node.nodeId }}</span>
-                </div>
-                <div
-                  class="message-content-cid"
-                  v-else-if="searchData.type == 'CID'"
-                >
-                  <span class="message-cid">{{ searchData.cid.cid }}</span>
-                  <span>{{ searchData.cid.pinned ? "pin" : "unpin" }}</span>
-                </div>
-              </div>
-              <div v-if="isShow && !status" class="content">
-                <h2 class="no-results-title">No Results found.</h2>
-                <p class="no-results-content">
-                  Please search by Node ID,Block ID or CID
-                </p>
-              </div>
-            </v-menu>
-          </div>
-          <div
-            class="blance-container ml-7"
-            v-if="$store.state.account"
-            @click="handleClaim"
-          >
-            <p class="blance-content">
-              Available to Claim:
-              <span class="blance">{{ $store.state.balance }}</span>
-              4EVER
-            </p>
-          </div>
-          <div class="connect-wallet ml-7" v-else>
-            <img
-              style="width: 24px"
-              src="../assets/imgs/header/link-icon.png"
-              alt=""
-            />
-            <span @click="connectWallet">Connect Wallet</span>
-          </div>
-        </div>
-        <v-dialog class="rounded-xl" v-model="claimDialog" width="500">
-          <v-card class="pl-7 pr-7 pb-11 rounded-xl">
-            <v-card-title class="pa-0 justify-center mt-8">
-              <template>
-                <div>My Reward</div>
-              </template>
-            </v-card-title>
-            <v-card-text class="text-center mt-10 pb-14">
-              <span class="claim-blance">{{ $store.state.balance }}</span>
-              <span class="currency">4EVER</span>
-            </v-card-text>
-            <v-card-actions style="width: 100%">
-              <v-btn
-                height="48"
-                class="claim-btn"
-                block
-                @click="handleClaimConfirm"
-              >
-                <span>Claim</span>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-container>
-    </v-app-bar>
-  </div>
+	<div>
+		<v-app-bar fixed app color="#FAFAFA" height="100">
+			<v-container class="d-flex align-center justify-space-between">
+				<v-btn
+					text
+					color="transparent"
+					to="/"
+					class="always-active mr-8"
+				>
+					<logo />
+				</v-btn>
+				<div class="d-flex align-center justify-space-between">
+					<div class="search-bar">
+						<input
+							class="search-input"
+							type="text"
+							v-model="searchValue"
+							@keydown.enter="handleSearch"
+							placeholder="Node/TeeReport/CID......"
+						/>
+						<v-menu
+							:value="isShow"
+							:close-on-content-click="false"
+							@input="handleMenu"
+							transition="slide-y-transition"
+							attach
+							nudge-bottom="50"
+						>
+							<div v-if="isShow && status" class="content">
+								<div class="message-header d-flex align-center">
+									<i class="header-line left"></i>
+									<span>{{
+										searchData.type == "BLOCK"
+											? "TeeReport"
+											: searchData.type == "CID"
+											? "CID"
+											: "NodeID"
+									}}</span>
+									<i class="header-line right"></i>
+								</div>
+								<div
+									class="message-content-block d-flex flex-column"
+									v-if="searchData.type == 'BLOCK'"
+								>
+									<span class="block-info"
+										>#
+										{{ searchData.block.blockNumber }}</span
+									>
+									<span class="block-time">
+										{{ searchData.block.createdAt }}</span
+									>
+								</div>
+								<div
+									class="message-content-cid"
+									v-else-if="searchData.type == 'NODE'"
+								>
+									<span class="message-cid">{{
+										searchData.node.nodeId
+									}}</span>
+								</div>
+								<div
+									class="message-content-cid"
+									v-else-if="searchData.type == 'CID'"
+								>
+									<span class="message-cid">{{
+										searchData.cid.cid
+									}}</span>
+									<span>{{
+										searchData.cid.pinned ? "pin" : "unpin"
+									}}</span>
+								</div>
+							</div>
+							<div v-if="isShow && !status" class="content">
+								<h2 class="no-results-title">
+									No Results found.
+								</h2>
+								<p class="no-results-content">
+									Please search by Node ID,TeeReport ID or CID
+								</p>
+							</div>
+						</v-menu>
+					</div>
+					<div
+						class="blance-container ml-7"
+						v-if="$store.state.account"
+						@click="handleClaim"
+					>
+						<p class="blance-content">
+							Available to Claim:
+							<span class="blance">{{
+								$store.state.balance
+							}}</span>
+							4EVER
+						</p>
+					</div>
+					<div class="connect-wallet ml-7" v-else>
+						<img
+							style="width: 24px"
+							src="../assets/imgs/header/link-icon.png"
+							alt=""
+						/>
+						<span @click="connectWallet">Connect Wallet</span>
+					</div>
+				</div>
+				<v-dialog class="rounded-xl" v-model="claimDialog" width="500">
+					<v-card class="pl-7 pr-7 pb-11 rounded-xl">
+						<v-card-title
+							class="dialog-title pa-0 justify-center mt-8"
+						>
+							My Reward
+							<img
+								class="dialog-close"
+								src="../assets/imgs/nodeDetail/close.png"
+								alt=""
+								@click="claimDialog = false"
+							/>
+						</v-card-title>
+						<v-card-text class="text-center mt-10 pb-14">
+							<span class="claim-blance">{{
+								$store.state.balance
+							}}</span>
+							<span class="currency">4EVER</span>
+						</v-card-text>
+						<v-card-actions style="width: 100%">
+							<v-btn
+								height="48"
+								class="claim-btn"
+								block
+								@click="handleClaimConfirm"
+							>
+								<span>Claim</span>
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
+			</v-container>
+		</v-app-bar>
+	</div>
 </template>
 
 <script>
@@ -121,256 +145,274 @@ import Logo from "../components/Logo.vue";
 import { mapActions, mapMutations } from "vuex";
 import { fetchSearchValue } from "@/api/api";
 export default {
-  components: { Logo },
-  data() {
-    return {
-      rewards: 0,
-      isLogin: false,
-      isShow: false,
-      searchValue: "",
-      searchData: {},
-      status: false,
-      claimDialog: false,
-    };
-  },
-  created() {
-    if (!window.ethereum) return;
-    this.updateChainId();
-    this.getAccount();
-    this.listen();
-  },
-  methods: {
-    ...mapActions([
-      "getAccount",
-      "updateAccount",
-      "updateChainId",
-      "updateBalance",
-      "claim",
-    ]),
-    ...mapMutations(["UPDATE_ACCOUNT", "UPDATE_BALANCE"]),
-    async connectWallet() {
-      if (!window.ethereum) {
-        window.open("https://metamask.io/download.html", "_blank");
-        return this.$message.warning("please download metamask");
-      }
-      const isUnlocked = await window.ethereum._metamask.isUnlocked();
-      if (!isUnlocked) {
-        return this.$message.warning("please lock metamask");
-      }
-      await this.updateAccount();
-    },
-    handleClaim() {
-      if (this.$store.state.balance == 0)
-        return this.$message.error("Insufficient account balance！");
-      this.claimDialog = true;
-    },
-    async handleClaimConfirm() {
-      this.claimDialog = false;
-      this.$loading.show();
-      try {
-        const result = await this.claim();
-        this.updateBalance();
-        this.$message.success("withDraw success!");
-      } catch (error) {
-        this.$message.error("withDraw fail!");
-      }
-      this.$loading.hide();
-    },
-    async handleSearch() {
-      try {
-        this.searchValue = this.searchValue.trim();
-        const reg = /^[A-Za-z0-9]{1,255}$/;
-        if (!reg.test(this.searchValue)) {
-          return this.$message.warning("Please enter letters or numbers！");
-        }
-        const result = await fetchSearchValue(this.searchValue);
-        if (result.type.toUpperCase() == "BLOCK") {
-          result.block.createdAt = formart_date(result.block.createdAt);
-        }
-        this.searchData = result;
-        console.log(result);
-        this.status = true;
-      } catch (e) {
-        console.log("err", e);
-      }
-      this.isShow = true;
-    },
-    handleMenu(e) {
-      console.log(e);
-      this.status = false;
-      this.isShow = false;
-    },
-    listen() {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        window.location.reload();
-      });
-      window.ethereum.on("chainChanged", (chainId) => {
-        window.location.reload();
-      });
-    },
-  },
+	components: { Logo },
+	data() {
+		return {
+			rewards: 0,
+			isLogin: false,
+			isShow: false,
+			searchValue: "",
+			searchData: {},
+			status: false,
+			claimDialog: false,
+		};
+	},
+	created() {
+		if (!window.ethereum) return;
+		this.updateChainId();
+		this.getAccount();
+		this.listen();
+	},
+	methods: {
+		...mapActions([
+			"getAccount",
+			"updateAccount",
+			"updateChainId",
+			"updateBalance",
+			"claim",
+		]),
+		...mapMutations(["UPDATE_ACCOUNT", "UPDATE_BALANCE"]),
+		async connectWallet() {
+			if (!window.ethereum) {
+				window.open("https://metamask.io/download.html", "_blank");
+				return this.$message.warning("please download metamask");
+			}
+			const isUnlocked = await window.ethereum._metamask.isUnlocked();
+			if (!isUnlocked) {
+				return this.$message.warning("please lock metamask");
+			}
+			await this.updateAccount();
+		},
+		handleClaim() {
+			if (this.$store.state.balance == 0)
+				return this.$message.error("Insufficient account balance！");
+			this.claimDialog = true;
+		},
+		async handleClaimConfirm() {
+			this.claimDialog = false;
+			this.$loading.show();
+			try {
+				const result = await this.claim();
+				this.updateBalance();
+				this.$message.success("withDraw success!");
+			} catch (error) {
+				this.$message.error("withDraw fail!");
+			}
+			this.$loading.hide();
+		},
+		async handleSearch() {
+			try {
+				this.searchValue = this.searchValue.trim();
+				const reg = /^[A-Za-z0-9]{1,255}$/;
+				if (!reg.test(this.searchValue)) {
+					return this.$message.warning(
+						"Please enter letters or numbers！"
+					);
+				}
+				const result = await fetchSearchValue(this.searchValue);
+				if (result.type.toUpperCase() == "BLOCK") {
+					result.block.createdAt = formart_date(
+						result.block.createdAt
+					);
+				}
+				this.searchData = result;
+				console.log(result);
+				this.status = true;
+			} catch (e) {
+				console.log("err", e);
+			}
+			this.isShow = true;
+		},
+		handleMenu(e) {
+			console.log(e);
+			this.status = false;
+			this.isShow = false;
+		},
+		listen() {
+			window.ethereum.on("accountsChanged", (accounts) => {
+				window.location.reload();
+			});
+			window.ethereum.on("chainChanged", (chainId) => {
+				window.location.reload();
+			});
+		},
+	},
 };
 </script>
 <style lang="less" scoped>
 .always-active {
-  opacity: 1 !important;
+	opacity: 1 !important;
 }
 .v-btn--active,
 .nav-btn:hover {
-  color: #2a7eed;
+	color: #2a7eed;
 }
 /deep/ .v-btn__content {
-  opacity: 1 !important;
+	opacity: 1 !important;
 }
 /deep/ .nav-btn .v-btn__content {
-  opacity: 1 !important;
+	opacity: 1 !important;
 }
 /deep/ .v-dialog {
-  border-radius: 24px;
+	border-radius: 24px;
 }
 .v-card {
-  overflow: hidden;
+	overflow: hidden;
 }
 .v-list {
-  width: 161px;
+	width: 161px;
 }
 .v-list-item__title {
-  color: red;
+	color: red;
 }
 .search-bar {
-  position: relative;
-  width: 297px;
-  .search-input {
-    position: relative;
-    width: 296px;
-    padding: 0 20px;
-    border-bottom: 1px solid black;
-    box-sizing: border-box;
-  }
+	position: relative;
+	width: 297px;
+	.search-input {
+		position: relative;
+		width: 296px;
+		padding: 0 20px;
+		border-bottom: 1px solid black;
+		box-sizing: border-box;
+	}
 
-  .message-tips {
-    position: absolute;
-    width: 100%;
-    left: 0;
-    bottom: 0;
-    // margin-top: 20px;
-  }
-  .content {
-    padding: 0 12px;
-    min-width: 297px;
-    // border-radius: 8px;
-    box-sizing: border-box;
-    background: #fff;
-    overflow: hidden;
-  }
-  .message-header {
-    margin-top: 15px;
-    .header-line {
-      display: inline-block;
-      height: 1px;
-      background: #e6e8eb;
-    }
-    span {
-      padding: 0 13px;
-    }
-    .header-line.left {
-      width: 16px;
-    }
-    .header-line.right {
-      flex: 1;
-    }
-  }
-  .message-content-cid {
-    padding: 23px 0;
-    font-size: 14px;
-    color: #495667;
-    line-height: 16px;
-    .message-cid {
-      margin-right: 62px;
-    }
-  }
-  .message-content-block {
-    min-width: 100%;
-    padding: 13px 0;
-    .block-info {
-      font-size: 16px;
-      color: #34a9ff;
-      line-height: 18px;
-    }
-    .block-time {
-      margin-top: 4px;
-      font-size: 14px;
-      line-height: 16px;
-      color: #7c848c;
-    }
-  }
-  .no-results-title {
-    padding-top: 19px;
-    font-size: 16px;
-    color: #495667;
-    line-height: 18px;
-  }
-  .no-results-content {
-    margin: 0;
-    font-size: 14px;
-    color: #7c848c;
-    line-height: 16px;
-    padding: 10px 0 18px 0;
-  }
+	.message-tips {
+		position: absolute;
+		width: 100%;
+		left: 0;
+		bottom: 0;
+		// margin-top: 20px;
+	}
+	.content {
+		padding: 0 12px;
+		min-width: 297px;
+		// border-radius: 8px;
+		box-sizing: border-box;
+		background: #fff;
+		overflow: hidden;
+	}
+	.message-header {
+		margin-top: 15px;
+		.header-line {
+			display: inline-block;
+			height: 1px;
+			background: #e6e8eb;
+		}
+		span {
+			padding: 0 13px;
+		}
+		.header-line.left {
+			width: 16px;
+		}
+		.header-line.right {
+			flex: 1;
+		}
+	}
+	.message-content-cid {
+		padding: 23px 0;
+		font-size: 14px;
+		color: #495667;
+		line-height: 16px;
+		.message-cid {
+			margin-right: 62px;
+		}
+	}
+	.message-content-block {
+		min-width: 100%;
+		padding: 13px 0;
+		.block-info {
+			font-size: 16px;
+			color: #34a9ff;
+			line-height: 18px;
+		}
+		.block-time {
+			margin-top: 4px;
+			font-size: 14px;
+			line-height: 16px;
+			color: #7c848c;
+		}
+	}
+	.no-results-title {
+		padding-top: 19px;
+		font-size: 16px;
+		color: #495667;
+		line-height: 18px;
+	}
+	.no-results-content {
+		margin: 0;
+		font-size: 14px;
+		color: #7c848c;
+		line-height: 16px;
+		padding: 10px 0 18px 0;
+	}
 }
 .search-bar::before {
-  content: "";
-  display: block;
-  position: absolute;
-  left: 0;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 16px;
-  height: 16px;
-  background: url("../assets/imgs/header/search-icon.png") no-repeat;
-  background-size: 100%;
+	content: "";
+	display: block;
+	position: absolute;
+	left: 0;
+	top: 50%;
+	transform: translateY(-50%);
+	width: 16px;
+	height: 16px;
+	background: url("../assets/imgs/header/search-icon.png") no-repeat;
+	background-size: 100%;
 }
 .connect-wallet {
-  padding: 5px 11px 5px 6px;
-  border: 1px solid #34a9ff;
-  border-radius: 2px;
-  font-size: 14px;
-  color: #34a9ff;
-  line-height: 20px;
-  cursor: pointer;
-  img {
-    vertical-align: middle;
-    margin-right: 6px;
-  }
+	padding: 5px 11px 5px 6px;
+	border: 1px solid #34a9ff;
+	border-radius: 2px;
+	font-size: 14px;
+	color: #34a9ff;
+	line-height: 20px;
+	cursor: pointer;
+	img {
+		vertical-align: middle;
+		margin-right: 6px;
+	}
 }
 .blance-container {
-  padding: 5px 15px;
-  background: #34a9ff;
-  border-radius: 4px;
-  cursor: pointer;
-  .blance-content {
-    color: #fff;
-    margin-bottom: 0;
-    font-size: 12px;
-    .blance {
-      font-size: 18px;
-    }
-  }
+	padding: 5px 15px;
+	background: #34a9ff;
+	border-radius: 4px;
+	cursor: pointer;
+	.blance-content {
+		color: #fff;
+		margin-bottom: 0;
+		font-size: 12px;
+		.blance {
+			font-size: 18px;
+		}
+	}
+}
+.dialog-title {
+	position: relative;
+	font-size: 18px;
+	font-weight: 600;
 }
 .claim-blance {
-  font-size: 40px;
-  color: #34a9ff;
+	font-size: 40px;
+	color: #34a9ff;
 }
 .currency {
-  margin-left: 10px;
-  font-size: 24px;
+	margin-left: 10px;
+	font-size: 24px;
 }
 .v-btn {
-  color: #fff;
+	color: #fff;
 }
 .claim-btn {
-  color: #fff !important;
-  font-weight: bold;
-  background: #34a9ff !important;
+	color: #fff !important;
+	font-weight: bold;
+	background: #34a9ff !important;
+}
+.dialog-close {
+	position: absolute;
+	right: 30px;
+	top: 50%;
+	transform: translateY(-50%);
+	width: 18px;
+	vertical-align: middle;
+	cursor: pointer;
 }
 </style>
--- src/components/NetworkBubble.vue
+++ src/components/NetworkBubble.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bubble-container" style="height: 100%">
-    <div id="map" style="position: relative; height: 100%"></div>
-  </div>
+	<div class="bubble-container" style="height: 100%">
+		<div id="map" style="position: relative; height: 100%"></div>
+	</div>
 </template>
 
 <script>
@@ -9,188 +9,162 @@ import { Scene, PointLayer } from "@antv/l7";
 import { Mapbox } from "@antv/l7-maps";
 
 export default {
-  data() {
-    return {
-      scene: null,
-      pointLayer: null,
-      maxSize: 100,
-    };
-  },
-  created() {},
-  mounted() {
-    this.init();
-  },
-  methods: {
-    init() {
-      const map = new Mapbox({
-        pitch: 0,
-        style: "light",
-        center: [96.99215001469588, 29.281597225674773],
-        zoom: 2,
-        maxZoom: 10,
-      });
-      this.scene = new Scene({
-        id: "map",
-        map,
-      });
-
-      // this.scene.on("loaded", () => {
-      //   fetch(
-      //     "https://gw.alipayobjects.com/os/basement_prod/337ddbb7-aa3f-4679-ab60-d64359241955.json"
-      //   )
-      //     .then((res) => res.json())
-      //     .then((data) => {
-      //       data.features = data.features.filter((item) => {
-      //         return item.properties.capacity > 4000;
-      //       });
-      //       console.log(data);
-      //       const pointLayer = new PointLayer({})
-      //         .source(data)
-      //         .shape("circle")
-      //         .size("capacity")
-      //         .color("capacity", ["#ABCAEA", "#ABCAEA", "#ABCAEA"])
-      //         .animate(true)
-      //         .active(true)
-      //         .style({
-      //           opacity: 1,
-      //           strokeWidth: 0,
-      //         });
-      //       this.scene.addLayer(pointLayer);
-      //     });
-      // });
-
-      this.scene.on("loaded", () => {
-        const data = {
-          features: [
-            {
-              geometry: {
-                coordinates: [121.47, 31.24],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "China",
-                plant: "ShangHai",
-                color: "#ABCAEA",
-              },
-            },
-            {
-              geometry: {
-                coordinates: [-0.12, 51.51],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "United Kingdom",
-                plant: "London",
-                color: "#ABCAEA",
-              },
-            },
-            {
-              geometry: {
-                coordinates: [-123.11, 49.33],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "Canada",
-                plant: "Vancouver",
-                color: "#ABCAEA",
-              },
-            },
-            {
-              geometry: {
-                coordinates: [-73.96, 40.73],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "United States",
-                plant: "New York",
-                color: "#ABCAEA",
-              },
-            },
-            {
-              geometry: {
-                coordinates: [114.17, 22.33],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "China",
-                plant: "Hong Kong",
-                color: "#ABCAEA",
-              },
-            },
-            {
-              geometry: {
-                coordinates: [126.99, 37.55],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "Korea",
-                plant: "Seoul",
-                color: "#ABCAEA",
-              },
-            },
-            {
-              geometry: {
-                coordinates: [151.24, -33.75],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "Australia",
-                plant: "Sydney",
-                color: "#ABCAEA",
-              },
-            },
-            {
-              geometry: {
-                coordinates: [13.41, 52.52],
-                type: "Point",
-              },
-              properties: {
-                capacity: 1000,
-                country: "Deutschland",
-                plant: "Berlin",
-                color: "#ABCAEA",
-              },
-            },
-          ],
-          type: "FeatureCollection",
-        };
-        this.pointLayer = new PointLayer({})
-          .source(data)
-          .shape("circle")
-          .size("capacity", [10, 100])
-          .color("color")
-          .animate(true)
-          .active(true)
-          .style({
-            opacity: 1,
-            stroke: "red",
-            strokeWidth: 20,
-          });
-        this.scene.addLayer(this.pointLayer);
-      });
-      this.scene.on("zoomchange", (e) => {
-        const currentZoom = this.scene.getZoom().toFixed(2);
-        let maxSize = currentZoom * 50;
-        this.pointLayer.size("capacity", [10, maxSize]);
-        this.pointLayer.render();
-      });
-    },
-  },
+	data() {
+		return {
+			scene: null,
+			pointLayer: null,
+			maxSize: 100,
+		};
+	},
+	created() {},
+	mounted() {
+		this.init();
+	},
+	methods: {
+		init() {
+			const map = new Mapbox({
+				pitch: 0,
+				style: "mapbox://styles/mapbox/navigation-night-v1",
+				center: [96.99215001469588, 29.281597225674773],
+				zoom: 2,
+				maxZoom: 10,
+			});
+			this.scene = new Scene({
+				id: "map",
+				map,
+			});
+			this.scene.on("loaded", () => {
+				const data = {
+					features: [
+						{
+							geometry: {
+								coordinates: [121.47, 31.24],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "China",
+								plant: "ShangHai",
+								color: "#ABCAEA",
+							},
+						},
+						{
+							geometry: {
+								coordinates: [-0.12, 51.51],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "United Kingdom",
+								plant: "London",
+								color: "#ABCAEA",
+							},
+						},
+						{
+							geometry: {
+								coordinates: [-123.11, 49.33],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "Canada",
+								plant: "Vancouver",
+								color: "#ABCAEA",
+							},
+						},
+						{
+							geometry: {
+								coordinates: [-73.96, 40.73],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "United States",
+								plant: "New York",
+								color: "#ABCAEA",
+							},
+						},
+						{
+							geometry: {
+								coordinates: [114.17, 22.33],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "China",
+								plant: "Hong Kong",
+								color: "#ABCAEA",
+							},
+						},
+						{
+							geometry: {
+								coordinates: [126.99, 37.55],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "Korea",
+								plant: "Seoul",
+								color: "#ABCAEA",
+							},
+						},
+						{
+							geometry: {
+								coordinates: [151.24, -33.75],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "Australia",
+								plant: "Sydney",
+								color: "#ABCAEA",
+							},
+						},
+						{
+							geometry: {
+								coordinates: [13.41, 52.52],
+								type: "Point",
+							},
+							properties: {
+								capacity: 1000,
+								country: "Deutschland",
+								plant: "Berlin",
+								color: "#ABCAEA",
+							},
+						},
+					],
+					type: "FeatureCollection",
+				};
+				this.pointLayer = new PointLayer({})
+					.source(data)
+					.shape("circle")
+					.size("capacity", [10, 100])
+					.color("color")
+					.animate(true)
+					.active(true)
+					.style({
+						opacity: 1,
+						stroke: "red",
+						strokeWidth: 20,
+					});
+				this.scene.addLayer(this.pointLayer);
+			});
+			this.scene.on("zoomchange", (e) => {
+				const currentZoom = this.scene.getZoom().toFixed(2);
+				let maxSize = currentZoom * 50;
+				this.pointLayer.size("capacity", [10, maxSize]);
+				this.pointLayer.render();
+			});
+		},
+	},
 };
 </script>
 
 <style lang="less" scoped>
 .bubble-container {
-  #id {
-    height: 100%;
-    position: relative;
-  }
+	#id {
+		height: 100%;
+		position: relative;
+	}
 }
 </style>
